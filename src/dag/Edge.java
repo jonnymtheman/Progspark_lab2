cd8da5db371cd77f043d81daa991660768f1254b
Jonas Nyman <c13jnn@cs.umu.se>
2016-01-27 15:10:55 +0100

    Aä

--- src/dag/Edge.java
+++ src/dag/Edge.java
@@ -3,12 +3,19 @@ package dag;
 
 class Edge {
 
-    private Vertex a, b;
+    private Vertex origin, destination;
     Object weight;
 
     public Edge(Vertex a, Vertex b, Object weight) {
-
+        origin = a;
+        destination = b;
+        this.weight = weight;
     }
 
+    protected Vertex getOrigin() { return origin; }
 
+
+    protected Vertex getDestination() { return destination; }
+
+    protected Object getWeight() { return weight; }
 }
--- src/dag/Vertex.java
+++ src/dag/Vertex.java
@@ -1,19 +1,13 @@
 package dag;
 
-import dag.Edge;
-
 class Vertex {
     private Object weight;
-    //private Edge edge;
     protected boolean visited;
 
     protected Vertex(Object weight){
         this.weight = weight;
     }
 
-    protected void setWeight(int w){
-        this.weight = w;
-    }
 
     protected Object getWeight(){
         return weight;
@@ -22,4 +16,5 @@ class Vertex {
     protected  int getId() {
         return this.hashCode();
     }
+
 }
