d0892c5fdeb29139c99db060412e1bcd1addd81b
Bjorn Lidell <c13bll@cs.umu.se>
2016-03-11 17:52:12 +0100

    Folders..

---
+++ src/tests/VertexTest.java
@@ -0,0 +1,32 @@
+package dag;
+
+import dag.Vertex;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VertexTest {
+    private Vertex v1;
+    private Vertex v2;
+
+    @Before
+    public void setUp() throws Exception {
+         v1 = new Vertex(11);
+         v2 = new Vertex(11);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testSetWeight() throws Exception {
+            assertNotEquals(v1.getId(), v2.getId());
+            assertNotEquals(true,v1.equals(v2));
+            assertNotEquals(v1.getId(), v2.getId());
+            assertNotEquals(true,v1.equals(v2));
+    }
+}
